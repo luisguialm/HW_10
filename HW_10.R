f78ef2a20e2403f9d84148b3622c7ab088de1b54
luisguialm <luisguialm@gmail.com>
2019-11-08 11:23:46 -0500

    fixed issues:

--- HW_10.R
+++ HW_10.R
@@ -1,15 +1,14 @@
-co2perCap<-read.csv("data/co2_emissions_tonnes_per_person_gapminder.csv")
-
-co2_report<-function(dat,country=NULL, year=NULL){
-   m<-paste0("X",year)
- stopifnot(any(names(dat[,2:ncol(dat)])==m)) # line to avoid years or countries that are not present in the data set.  
- stopifnot(any(country==dat$country))
+co2_report<-function(country=NULL, year=NULL){
+  dat<-read.csv("data/co2_emissions_tonnes_per_person_gapminder.csv")
+  m<-paste0("X",year)
+ # stopifnot(any(names(dat[,2:ncol(dat)])==m)) # line to avoid years or countries that are not present in the data set.  
+ # stopifnot(any(country==dat$country))
   if (!is.null(country)) {
     value <- dat[dat$country %in% country,names(dat)==m]
-    all_countries<-  dat[,names(dat)==m]
-  }  
-  stopifnot(is.nan(value))
-  stopifnot(is.na(value))
+    all_countries<-dat[,names(dat)==m]
+  }
+ #  stopifnot(is.nan(value))
+ #  stopifnot(is.na(value))
 
   # A - Bottom 10% of emitters in that year  
   # B- Bottom 11-20% emitters in that year   
@@ -17,13 +16,13 @@ co2_report<-function(dat,country=NULL, year=NULL){
   # D- Bottom 31-40% emitters in that year   
   # F- Greater than 40% of emitters in that year   
   
-  if(value>quantile(all_countries,0.4)){
+  if(value>quantile(na.omit(all_countries),0.4)){
      grade<-"F"
-  }else if(value>quantile(all_countries,0.3)){
+  }else if(value>quantile(na.omit(all_countries),0.3,rm.na=TRUE)){
      grade<-"D"
-  }else if(value>quantile(all_countries,0.2)){
+  }else if(value>quantile(na.omit(all_countries),0.2)){
         grade<-"C"
-  }else if(value>quantile(all_countries,0.1)){
+  }else if(value>quantile(na.omit(all_countries),0.1)){
         grade<-"B"
   }else{
           grade<-"A"  
@@ -32,4 +31,4 @@ co2_report<-function(dat,country=NULL, year=NULL){
   paste("Grade: ",grade,"In",year,country,"had",value,"tonnes CO2 per person per year")
 }
 
-co2_report(dat=co2perCap,country="United States",year=)
+
